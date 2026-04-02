41ba61f2e1572900f99f46fa00eb9f9afa6ca061
w-chuhao <e1398566@u.nus.edu>
2026-04-02 13:06:05 +0800

    Lastest update, tempt sensor not working

--- source/main/main.ino
+++ source/main/main.ino
@@ -1,13 +1,51 @@
 #include <Arduino.h>
 #include "uart_receive.h"
+#include "sensors.h"
+
+DeskState espDesk = {0.0f, 0.0f, 0.0f, 0, false, true, OFF, 0};
+uint32_t lastEspPrintMs = 0;
+
+static void printEspSensors() {
+  Serial.print("ESP DIST_CM=");
+  if (espDesk.distance < 0) {
+    Serial.print("ERR");
+  } else {
+    Serial.print(espDesk.distance, 2);
+  }
+
+  Serial.print(" | HUMIDITY=");
+  if (isnan(espDesk.humidity)) {
+    Serial.print("ERR");
+  } else {
+    Serial.print(espDesk.humidity, 1);
+    Serial.print("%");
+  }
+
+  Serial.print(" | TEMP_C=");
+  if (isnan(espDesk.temp)) {
+    Serial.println("ERR");
+  } else {
+    Serial.print(espDesk.temp, 1);
+    Serial.println("C");
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   delay(300);
+
   uartReceiveInit();
+  sensorsInit();
+
   Serial.println("Listening for MCXC packets on Serial1...");
 }
 
 void loop() {
   uartReceiveLoop();
+
+  if (millis() - lastEspPrintMs >= 1000UL) {
+    lastEspPrintMs = millis();
+    sensorsRead(espDesk);
+    printEspSensors();
+  }
 }
